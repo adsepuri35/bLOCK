4faeb72c20fa665e4befbab9eba8fec8c66dbd18
adsepuri35 <advaitsepuri@gmail.com>
2023-04-22 20:33:27 -0400

    camera dependencies

--- myapp/lib/pages/auth_page.dart
+++ myapp/lib/pages/auth_page.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/pages/login_or_register_page.dart';
-import 'package:myapp/pages/login_page.dart';
 import 'package:myapp/pages/home_page.dart';
 
 class AuthPage extends StatelessWidget {
