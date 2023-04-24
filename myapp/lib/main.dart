d6feb9702f98daf85b46c60afb4fb9230c43c9ce
adsepuri35 <advaitsepuri@gmail.com>
2023-04-24 19:57:32 -0400

    camera testing

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:myapp/firebase_options.dart';
 import 'package:myapp/pages/auth_page.dart';
-import 'pages/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
