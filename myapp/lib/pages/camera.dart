ace76b7a0de30a5acc7698654245b01fcff4b9c5
adsepuri35 <advaitsepuri@gmail.com>
2023-05-01 22:00:31 -0400

    camera button

--- myapp/lib/pages/camera.dart
+++ myapp/lib/pages/camera.dart
@@ -53,54 +53,64 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       // You must wait until the controller is initialized before displaying the
       // camera preview. Use a FutureBuilder to display a loading spinner until the
       // controller has finished initializing.
-      body: Column(
-        children: [
-          FutureBuilder<void>(
-            future: _initializeControllerFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                // If the Future is complete, display the preview.
-                return CameraPreview(_controller);
-              } else {
-                // Otherwise, display a loading indicator.
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        // Provide an onPressed callback.
-        onPressed: () async {
-          // Take the Picture in a try / catch block. If anything goes wrong,
-          // catch the error.
-          try {
-            // Ensure that the camera is initialized.
-            await _initializeControllerFuture;
-
-            // Attempt to take a picture and get the file `image`
-            // where it was saved.
-            final image = await _controller.takePicture();
-
-            if (!mounted) return;
-
-            // If the picture was taken, display it on a new screen.
-            await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => BlockchainConfirmationScreen(
-                  // Pass the automatically generated path to
-                  // the DisplayPictureScreen widget.
-                  imagePath: image.path,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              FutureBuilder<void>(
+                future: _initializeControllerFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    // If the Future is complete, display the preview.
+                    return CameraPreview(_controller);
+                  } else {
+                    // Otherwise, display a loading indicator.
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                },
+              ),
+              SizedBox(
+                width: 56.0,
+                height: 56.0,
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      // Take the Picture in a try / catch block. If anything goes wrong,
+                      // catch the error.
+                      try {
+                        // Ensure that the camera is initialized.
+                        await _initializeControllerFuture;
+        
+                        // Attempt to take a picture and get the file `image`
+                        // where it was saved.
+                        final image = await _controller.takePicture();
+        
+                        if (!mounted) return;
+        
+                        // If the picture was taken, display it on a new screen.
+                        await Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => BlockchainConfirmationScreen(
+                              // Pass the automatically generated path to
+                              // the DisplayPictureScreen widget.
+                              imagePath: image.path,
+                            ),
+                          ),
+                        );
+                      } catch (e) {
+                        // If an error occurs, log the error to the console.
+                        print(e);
+                      }
+                    },
+                    child: Center(child: Icon(Icons.camera_alt)),
+                  ),
                 ),
               ),
-            );
-          } catch (e) {
-            // If an error occurs, log the error to the console.
-            print(e);
-          }
-        },
-        child: const Icon(Icons.camera_alt),
+            ],
+          ),
+        ),
       ),
+      
       
     );
   }
