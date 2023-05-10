a867654e5ed804e335083f3a5875b0c9f1a72316
adsepuri35 <advaitsepuri@gmail.com>
2023-05-10 07:56:58 -0400

    home page formatting

--- myapp/lib/pages/home_page.dart
+++ myapp/lib/pages/home_page.dart
@@ -51,16 +51,27 @@ class _HomePageState extends State<HomePage> {
           child: Padding(
             padding:
                 const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
-            child: Column(
-              children: [
-                Text(
-                  'Recents',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
+            child: Center(
+              child: Column(
+                children: [
+                  Text(
+                    'IdeaVault',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
+                  Image.asset('lib/images/blockchain.png', width: 400, height: 250),
+                  Text(
+                    'IdeaVault is a blockchain-supported application that allows for engineers to securely store their documents for future use when filing for patents. The irreversible and distributed nature of the blockchain makes it nearly impossible for files to be altered in any way shape or form.',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
