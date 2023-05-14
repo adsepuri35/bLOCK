d10dce246d3efa10b313d081a6ae0cd13510e06a
adsepuri35 <advaitsepuri@gmail.com>
2023-05-13 21:08:56 -0400

    Recommend

--- myapp/lib/components/account_table.dart
+++ myapp/lib/components/account_table.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class AccountTable extends StatelessWidget {
   const AccountTable({super.key});
+
+  void _recommend() {
+    Share.share(
+        'Check out bLOCK! It\'s a efficient way to store documents and is powered by blockchain technology!');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -119,40 +125,43 @@ class AccountTable extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 1.0),
-          Container(
-            width: 350.0,
-            height: 50.0,
-            color: Colors.white,
-            padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 7,
-                        child: Icon(Icons.thumb_up),
-                      ),
-                      Positioned(
-                        left: 40,
-                        top: 10,
-                        child: Text(
-                          'Recommend',
-                          style: TextStyle(
-                            fontSize: 15,
+          GestureDetector(
+            onTap: _recommend,
+            child: Container(
+              width: 350.0,
+              height: 50.0,
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          top: 7,
+                          child: Icon(Icons.thumb_up),
+                        ),
+                        Positioned(
+                          left: 40,
+                          top: 10,
+                          child: Text(
+                            'Recommend',
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
                           ),
                         ),
-                      ),
-                      Positioned(
-                        right: 0,
-                        top: 7,
-                        child: Icon(Icons.arrow_forward_outlined),
-                      ),
-                    ],
+                        Positioned(
+                          right: 0,
+                          top: 7,
+                          child: Icon(Icons.arrow_forward_outlined),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
